ecefe9ffe065598fd2a88fccbbeb29b1e4d66258
Deborah Wei <dew354@nyu.edu>
2022-08-28 18:11:58 -0400

    Added splash backgrounds and text

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import SplashOneContainer from './splash-first-page'
+import SplashFirstPageContainer from './splash-first-page'
+import SplashInstructionPageContainer from './splash-instruction-page'
 
 const SplashContainer = () => {
 
     const content = () => {
         return (
         <div className='splash-container'>
-            <div className='splash-first-page-container'>
-                <SplashOneContainer/>
-            </div>
+            <section className='splash-first-page-container'>
+                <SplashFirstPageContainer/>
+            </section>
+            <section className='splash-second-page-container'>
+                <SplashInstructionPageContainer pageNumber={0} pageWord={"second"}/>
+            </section>
+            <section className='splash-third-page-container'>
+                <SplashInstructionPageContainer pageNumber={1} pageWord={"third"}/>
+            </section>
+            <section className='splash-fourth-page-container'>
+                <SplashInstructionPageContainer pageNumber={2} pageWord={"fourth"}/>
+            </section>
         </div>
         )
     }
---
+++ frontend/src/util/constants_util.js
@@ -0,0 +1,11 @@
+export const instructionsHeading = [
+    "Code better",
+    "Code faster", 
+    "Code together"
+]
+
+export const instructionsText = [
+    "Create rooms to work on problems with your friends. Our progress reports allow users to see their work as they progress",
+    "With our collobarative IDE tab, it allows users to code alongside one another. It keeps explaining concise and efficient",
+    "Using our live call and chat, it allows people to easily interact with each other and making coding together easier"
+]
