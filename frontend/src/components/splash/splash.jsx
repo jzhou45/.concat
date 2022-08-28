86c8375aa6850f96eec4cada0e9bfa6fa1dfe337
jzhou45 <jonathanzhou77@gmail.com>
2022-08-28 18:17:03 -0400

    add navbar skeleton and css

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import SplashOneContainer from './splash-first-page'
+import SplashNavBar from './splash_navbar'
 
 const SplashContainer = () => {
 
     const content = () => {
         return (
-        <div className='splash-container'>
-            <div className='splash-first-page-container'>
-                <SplashOneContainer/>
+            <div className='splash-container'>
+                <SplashNavBar />
+                <div className='splash-first-page-container'>
+                    <SplashOneContainer/>
+                </div>
             </div>
-        </div>
         )
     }
 
