a358ff80b78f1633ad5e0836b491190c82fe9805
Deborah Wei <dew354@nyu.edu>
2022-08-28 16:53:21 -0400

    Add variables folder and skeleton of splash css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import axios from 'axios';
+import './assets/stylesheets/App.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
