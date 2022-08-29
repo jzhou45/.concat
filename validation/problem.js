b57ea4bdd853715a369086e002df90e3c066774d
Victor Hoang <vikterhoang@gmail.com>
2022-08-29 10:58:35 -0400

    Finalize Problem Schema

--- validation/problem.js
+++ validation/problem.js
@@ -6,7 +6,12 @@ module.exports = function validateProblemInput(data) {
     let errors = {};
 
     data.title = validText(data.title) ? data.title : ''
-    data.title = validText(data.title) ? data.title : ''
-    data.title = validText(data.title) ? data.title : ''
+    data.description = validText(data.description) ? data.description : ''
+    data.testCase = validText(data.testCase) ? data.testCase : ''
+    data.solution = validText(data.solution) ? data.solution : ''
+    data.testCase2 = validText(data.testCase2) ? data.testCase2 : ''
+    data.solution2 = validText(data.solution2) ? data.solution2 : ''
+
+    
 
 };
