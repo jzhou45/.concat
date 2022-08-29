696534b7e503e467116234c7cc0e6699ff91b82f
jzhou45 <jonathanzhou77@gmail.com>
2022-08-29 10:41:50 -0400

    add signup to form modals

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import ui from "./ui_reducer";
+import session from "./session_reducer";
 
 const rootReducer = combineReducers({
-    ui
+    ui,
+    session
 });
 
 export default rootReducer;
