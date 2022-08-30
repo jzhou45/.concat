15868555c15501332be2f7750bbc1fe7674db09a
Alex Malos <malosalexander@gmail.com>
2022-08-30 09:55:11 -0400

    restored app.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,10 +7,19 @@ import Nav from './nav/nav'
 import Rooms from './rooms/rooms'
 import css from "../assets/stylesheets/App.scss"
 import Problems from './problems/problems';
-import Ide from './ide/ide';
 
 const App = () => (
-    <Ide />
+    <div>
+        <header>
+            <Nav/>
+        </header>
+        <Modal />
+        <Switch>
+            <ProtectedRoute exact path={"/rooms"} component={Rooms}/>
+            <ProtectedRoute exact path={"/rooms/:roomsId"} component={Problems}/>
+            <AuthRoute exact path={"/"} component={Splash} />
+        </Switch>
+    </div>
 );
 
 export default App;
