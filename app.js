69a4fae81faf5d8aee3920f10e7e6fa359626d87
jzhou45 <jonathanzhou77@gmail.com>
2022-08-30 18:10:24 -0400

    prepare for heroku

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const io = require('socket.io')(server, {
     origin:'*'
   }
 })
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+  })
+}
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
