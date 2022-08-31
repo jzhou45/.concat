5606947dcb73b7a430c7fcba9ee37ce68b8c5361
Victor Hoang <vikterhoang@gmail.com>
2022-08-31 13:56:13 -0400

    test ports

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react"
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+const socket = io(process.env.PORT)
 // const socket = io('https://concat-mern.herokuapp.com')
 
 const userName = 'User ' + parseInt(Math.random()*10)
