c1b9b0e2200b297938065540fd83a53de53a055d
Deborah Wei <dew354@nyu.edu>
2022-09-01 15:42:37 -0400

    Add link copied text to join room form

--- frontend/src/util/constants_util.js
+++ frontend/src/util/constants_util.js
@@ -9,3 +9,5 @@ export const instructionsText = [
     "Using our preset Leetcode questions and search bar allows you to just easily select one to work on, opening up that problem's own page.",
     "Each problem's page comes equipped with their own live chat and live IDE. This allows people to easily interact with each other and makes communication more efficient."
 ]
+
+export const demoUsername = "demo-user"
